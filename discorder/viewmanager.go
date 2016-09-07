72bcdae8ecafe832b9b7ca08a4e9d3971cd7e3a9
jonas747 <jonasr747@gmail.com>
2016-09-07 12:02:06 +0200

    Remove debug logging

--- discorder/viewmanager.go
+++ discorder/viewmanager.go
@@ -267,9 +267,7 @@ func (v *ViewManager) RemoveWindow(e ui.Entity) {
 
 	if len(v.menuContainer.Children(false)) > 0 {
 		v.menuContainer.Dynamic = true
-		log.Println("Dynamic is true")
 	} else {
-		log.Println("Dynamic is false")
 		v.menuContainer.Dynamic = false
 		v.menuContainer.Transform.Size = common.NewVector2I(0, 0)
 	}
