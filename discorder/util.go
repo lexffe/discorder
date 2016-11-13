2c97d6071eb8bf118aa9a5d3766ef179dc2d8aea
Condulum <lamwingka256@gmail.com>
2016-11-13 14:39:39 +0000

    Opens link with "open" in macOS
    (Note: in cmd/discorder/main.go, dependency should change to
    condulum/discorder if PR hasn’t gone through / PR is rejected.

--- discorder/util.go
+++ discorder/util.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"runtime"
 )
 
 var linkRegex = regexp.MustCompile(`(https?|ftp):\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)`)
@@ -174,9 +175,24 @@ func (app *App) GenMessageCommands(msg *discordgo.Message) []Command {
 }
 
 func (app *App) OpenLink(link string) {
-	cmd := exec.Command("xdg-open", link)
+
+  // Checks if it is in macOS
+	// In Windows, start [URL] might work, though it is not implemented here.
+
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+  case "darwin":
+		cmd = exec.Command("open", link)
+		break;
+	case "linux":
+	default:
+		cmd = exec.Command("xdg-open", link)
+		break;
+	}
+
 	err := cmd.Run()
 	if err != nil {
-		log.Println("Failed to run opening link with xdg-open", err)
+		log.Println("Failed to run opening link with xdg-open / open", err)
 	}
 }
